947339d28026a765ccf6f3c1fa2654af68a9a583
bimbomdon <dolbaeb.pidoras1703@gmail.com>
2026-09-15 20:48:21 +0300

    Add files via upload

---
+++ zad3.py
@@ -0,0 +1,7 @@
+celsius = float(input("Введите температуру в °C: "))
+# Вычисление для перевода градусов
+fahrenheit = celsius * 9 / 5 + 32
+kelvin = celsius + 273.15
+
+print(f"Температура в Фаренгейтах: {fahrenheit} °F")
+print(f"Температура в Кельвинах: {kelvin} K")
